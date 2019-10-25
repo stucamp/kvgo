8125704f05899e553cc6e63204bf7f75528ad4b1
Kevin Somers <ksomers3@asu.edu>
2019-10-24 22:01:10 -0400

    Initial commit

---
+++ server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"errors"
+	"net"
+	"sync"
+	"google.golang.org/grpc"
+	 pb "github.com/somerska/kvgo/api"
+)
+
+
+
+func main(){
+	//kvstore := make(map[string]string)
+	//mutex := sync.RWMutex{}
+	//safeKvStore := SafeMap {mutex, kvstore}
+
+}
+
+
